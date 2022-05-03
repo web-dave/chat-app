76e1285cd2cced853d6a6ced7208d66de0fe7566
web-dave <dave@webdave.de>
2022-05-03 22:12:11 +0200

    title dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'chat-app';
+  initTitle = '';
+  constructor(private service: Title) {
+    this.initTitle = service.getTitle();
+    service.setTitle(this.initTitle + ' ' + 'New stuff');
+  }
 }
