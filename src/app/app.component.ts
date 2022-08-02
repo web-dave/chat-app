95c9eddeaa34fe0e56c8a61bcef6ac2dcabe7774
web-dave <dave@webdave.de>
2022-08-02 22:03:56 +0200

    fixed msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,10 +16,11 @@ export class AppComponent implements OnInit {
   @ViewChild('stream') stream!: ElementRef<HTMLDivElement>;
   messages: IMsg[] = [];
   wss =
-    'wss://demo.piesocket.com/v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self';
+    'wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self';
   socket$$ = new WebSocketSubject<string>({
     url: this.wss,
-    deserializer: (e: MessageEvent) => e.data,
+    deserializer: (e: MessageEvent) =>
+      e.data.includes('{') ? JSON.parse(e.data) : e.data,
   });
   sendMsg(input: HTMLInputElement) {
     const myMsg: IMsg = {
@@ -27,7 +28,7 @@ export class AppComponent implements OnInit {
       msg: input.value,
     };
     this.socket$$.next(JSON.stringify(myMsg));
-    this.messages.push(myMsg);
+    // this.messages.push(myMsg);
 
     input.value = '';
     console.table(myMsg);
@@ -36,19 +37,22 @@ export class AppComponent implements OnInit {
     this.socket$$
       .pipe(
         filter((data) => data.includes('msg') && data.includes('name')),
-        map((data: string) => JSON.parse(data) as IMsg),
-        tap((data) => console.table(data))
+        tap((data) => console.log('1', data))
+        // map((data: string) => data.replace(/\\"/g, '"')),
+        // map((data: string) => data.substring(1, data.length - 1))
       )
       .subscribe((data) => {
-        if (data.msg) {
-          this.messages.push(data);
-          console.log(this.stream.nativeElement);
-          setTimeout(() => {
-            const div = this.stream.nativeElement;
+        console.log(data);
+        const obj = JSON.parse(data);
+        console.log(obj);
+        // console.log('3', { name: 'Dave', msg: 'sdfsfw' });
+        this.messages.push(obj);
+        // console.log(this.stream.nativeElement);
+        setTimeout(() => {
+          const div = this.stream.nativeElement;
 
-            div.scrollTop = div.scrollHeight;
-          }, 1);
-        }
+          div.scrollTop = div.scrollHeight;
+        }, 1);
       });
   }
 }
